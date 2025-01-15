acf3250cdf61353c5218400253d53fe5dbcc0bd2
Nadia Santalla <nadia@santalla.io>
2025-01-15 13:32:14 +0100

    create `http_version` metric

--- output.go
+++ output.go
@@ -237,6 +237,18 @@ func (ms *metricStore) DeriveMetrics() {
 				log.Tracef("Created %q from %q", statusCodeTS.name, ts.name)
 			}()
 
+			func() {
+				strCode, _ := ts.tags.Get("proto")
+				newValue, _ := strconv.ParseFloat(strings.TrimPrefix(strCode, "HTTP/"), 32)
+				httpVersionTS := timeseries{
+					name:       "http_version",
+					metricType: metrics.Gauge,
+					tags:       ts.tags,
+				}
+				ms.store[httpVersionTS] = value{newValue, 1}
+				log.Tracef("Created %q from %q", httpVersionTS.name, ts.name)
+			}()
+
 		// Rename to http_requests_failed_total
 		case "http_req_failed":
 			newTS := ts
