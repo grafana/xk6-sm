510f2ccf97c82168d55cf45cc4a34eb724b2367e
Nadia Santalla <969721+nadiamoe@users.noreply.github.com>
2025-02-14 11:44:49 +0100

    chore: integration: properly parse output metrics and assert more things about them (#44)
    * integration: embed test script, check for binary existence
    
    * integration: parse prometheus output and assert against it
    
    * integration: remove unused outer loops
    
    * integration: check for presence of more metrics `probe_checks_total`
    
    * integration: add test for unwanted metrics
    
    * integration: check for non-presence of more labels
    
    * integration: add text for presence of certain labels in certain metrics
    
    * integration: formatting
    
    * integration: add a check that succeeds, for a change
    
    * integration: add test asserting that certain metrics have certain values
    
    * integration: add test asserting metrics have the required prefix

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -3,13 +3,24 @@ package integration_test
 import (
 	"bytes"
 	"context"
+	_ "embed"
+	"errors"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"slices"
+	"strings"
 	"testing"
 	"time"
+
+	prometheus "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 )
+
+//go:embed test-script.js
+var testScript []byte
 
 func TestSMK6(t *testing.T) {
 	t.Parallel()
@@ -19,72 +30,304 @@ func TestSMK6(t *testing.T) {
 		smk6 = filepath.Join("..", "dist", "sm-k6-"+runtime.GOOS+"-"+runtime.GOARCH)
 	}
 
-	t.Run("metrics", func(t *testing.T) {
-		for _, tc := range []struct {
-			name   string
-			script []byte
-		}{
+	_, err := os.Stat(smk6)
+	if err != nil {
+		t.Fatalf("sm-k6 binary does not seem to exist, must be compiled before running this test: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	t.Cleanup(cancel)
+
+	outFile := filepath.Join(t.TempDir(), "metrics.txt")
+
+	cmd := exec.CommandContext(ctx, smk6, "run", "-", "-o=sm="+outFile)
+	cmd.Stdin = bytes.NewReader(testScript)
+	err = cmd.Run()
+	if err != nil {
+		t.Fatalf("running sm-k6: %v", err)
+	}
+
+	out, err := os.Open(outFile)
+	if err != nil {
+		t.Fatalf("reading output metrics: %v", err)
+	}
+
+	mfs := []*prometheus.MetricFamily{}
+	decoder := expfmt.NewDecoder(out, expfmt.FmtText)
+	for {
+		mf := &prometheus.MetricFamily{}
+		err := decoder.Decode(mf)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			t.Fatalf("decoding metric: %v", err)
+		}
+
+		mfs = append(mfs, mf)
+	}
+
+	if len(mfs) == 0 {
+		t.Fatalf("no metrics decoded")
+	}
+
+	t.Run("wanted metrics are present", func(t *testing.T) {
+		t.Parallel()
+
+		wantedMetrics := []string{
+			"probe_checks_total",
+			"probe_data_received_bytes",
+			"probe_data_sent_bytes",
+			"probe_http_duration_seconds",
+			"probe_http_error_code",
+			"probe_http_got_expected_response",
+			"probe_http_info",
+			"probe_http_requests_failed_total",
+			"probe_http_requests_total",
+			"probe_http_ssl",
+			"probe_http_status_code",
+			"probe_http_total_duration_seconds",
+			"probe_http_version",
+			"probe_iteration_duration_seconds",
+			"probe_script_duration_seconds",
+			// Custom metrics:
+			"probe_waiting_time", "probe_my_counter", "probe_my_gauge",
+		}
+
+		for _, wanted := range wantedMetrics {
+			if !slices.ContainsFunc(mfs, func(m *prometheus.MetricFamily) bool { return *m.Name == wanted }) {
+				t.Fatalf("Metric %q not found in output", wanted)
+			}
+		}
+	})
+
+	t.Run("unwanted metrics are not present", func(t *testing.T) {
+		t.Parallel()
+
+		unwantedMetrics := []string{
+			"probe_checks",
+			"probe_http_reqs", "probe_http_req_failed",
+			"probe_data_sent", "probe_data_received",
+			"probe_http_req_duration", "probe_iteration_duration",
+			"probe_http_req_blocked", "probe_http_req_connecting", "probe_http_req_receiving", "probe_http_req_sending", "probe_http_req_tls_handshaking", "probe_http_req_waiting",
+		}
+
+		for _, wanted := range unwantedMetrics {
+			if slices.ContainsFunc(mfs, func(m *prometheus.MetricFamily) bool { return *m.Name == wanted }) {
+				t.Fatalf("Metric %q not found in output", wanted)
+			}
+		}
+	})
+
+	t.Run("labels are present", func(t *testing.T) {
+		t.Parallel()
+
+		// requiredLabels maps metric names to label names that are required to be present on that metric.
+		requiredLabels := map[string][]string{
+			// FIXME: probe_http_info will not contain these labels if the request failed, so an instance of this metric
+			// fails this test.
+			//"probe_http_info":             {"tls_version", "proto"},
+			"probe_http_duration_seconds": {"phase"},
+			"probe_checks_total":          {"result"},
+		}
+
+		for _, mf := range mfs {
+			for _, m := range mf.Metric {
+				requiredLabelsForMetric := requiredLabels[*mf.Name]
+				for _, req := range requiredLabelsForMetric {
+					if !slices.ContainsFunc(m.Label, func(lp *prometheus.LabelPair) bool { return *lp.Name == req }) {
+						t.Fatalf("metric %q does not contain label %q", *mf.Name, req)
+					}
+				}
+			}
+		}
+	})
+
+	t.Run("labels are not present", func(t *testing.T) {
+		t.Parallel()
+
+		// The keys of this map are the set of labels that are not allowed to be present in most metrics.
+		// The values represent the list of metrics that are _allowed_ to have this label, as an exception.
+		// If a metric contains a label (key), and that metric is not in the list (value), the test fails.
+		type exceptForMetrics []string
+		forbiddenLabels := map[string]exceptForMetrics{
+			"tls_version":       {"probe_http_info"},
+			"proto":             {"probe_http_info"},
+			"error":             {"probe_http_info"},
+			"expected_response": {"probe_http_got_expected_response"},
+			"group":             {},
+		}
+
+		for _, mf := range mfs {
+			for _, m := range mf.Metric {
+				for _, labelPair := range m.Label {
+					allowedMetrics, isForbidden := forbiddenLabels[*labelPair.Name]
+					if !isForbidden {
+						continue
+					}
+
+					if !slices.Contains(allowedMetrics, *mf.Name) {
+						t.Fatalf("%q should not contain label %q", *mf.Name, *labelPair.Name)
+					}
+				}
+			}
+		}
+	})
+
+	t.Run("metrics have expected values", func(t *testing.T) {
+		t.Parallel()
+
+		type testCase struct {
+			name         string
+			metricName   string // Metric name to assert.
+			metricLabels map[string]string
+			assertValue  func(float64) bool
+		}
+
+		for _, tc := range []testCase{
+			// Some global metrics.
 			{
-				name:   "singleRequest",
-				script: scriptSingleRequest,
+				name:        "Script duration seconds",
+				metricName:  "probe_script_duration_seconds",
+				assertValue: nonZero,
+			},
+			{
+				name:        "Sent bytes",
+				metricName:  "probe_data_sent_bytes",
+				assertValue: nonZero,
+			},
+			{
+				name:        "Received bytes",
+				metricName:  "probe_data_received_bytes",
+				assertValue: nonZero,
+			},
+			// Check-related metrics.
+			{
+				name:         "Passed checks metric",
+				metricName:   "probe_checks_total",
+				metricLabels: map[string]string{"result": "pass"},
+				assertValue:  equals(1),
+			},
+			{
+				name:         "Failed checks metric",
+				metricName:   "probe_checks_total",
+				metricLabels: map[string]string{"result": "fail"},
+				assertValue:  equals(2),
+			},
+			// Misc http metrics.
+			{
+				name:         "HTTP duration second has a duration-ish value",
+				metricName:   "probe_http_duration_seconds",
+				metricLabels: map[string]string{"phase": "processing", "url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  nonZero,
+			},
+			{
+				name:         "HTTP duration seconds has custom 'resolve' phase",
+				metricName:   "probe_http_duration_seconds",
+				metricLabels: map[string]string{"phase": "resolve", "url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  any, // Just fail if not present.
+			},
+			{
+				name:         "Error code for request that should succeed",
+				metricName:   "probe_http_error_code",
+				metricLabels: map[string]string{"url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  equals(0),
+			},
+			{
+				name:         "Error code for request that should fail",
+				metricName:   "probe_http_error_code",
+				metricLabels: map[string]string{"url": "http://fail.internal/public/crocodiles4/"},
+				assertValue:  equals(1101),
+			},
+			{
+				name:         "HTTP status code for a request that should succeed",
+				metricName:   "probe_http_status_code",
+				metricLabels: map[string]string{"url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  equals(200),
+			},
+			{
+				name:         "Expected response for a request that should succeed",
+				metricName:   "probe_http_got_expected_response",
+				metricLabels: map[string]string{"url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  equals(1),
+			},
+			{
+				name:         "Expected response for a request that should fail",
+				metricName:   "probe_http_got_expected_response",
+				metricLabels: map[string]string{"url": "https://test-api.k6.io/public/crocodiles2/"},
+				assertValue:  equals(0),
+			},
+			{
+				name:        "Total requests for each url",
+				metricName:  "probe_http_requests_total",
+				assertValue: equals(1),
+			},
+			{
+				name:         "HTTP version",
+				metricName:   "probe_http_version",
+				metricLabels: map[string]string{"url": "https://test-api.k6.io/public/crocodiles/"},
+				assertValue:  func(f float64) bool { return f >= 1.1 },
 			},
 		} {
 			tc := tc
-
 			t.Run(tc.name, func(t *testing.T) {
-				t.Parallel()
-
-				ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-				t.Cleanup(cancel)
-
-				outFile := filepath.Join(t.TempDir(), "metrics.txt")
-
-				cmd := exec.CommandContext(ctx, smk6, "run", "-", "-o=sm="+outFile)
-				cmd.Stdin = bytes.NewReader(tc.script)
-				err := cmd.Run()
-				if err != nil {
-					t.Fatalf("running sm-k6: %v", err)
-				}
-
-				out, err := os.ReadFile(outFile)
-				if err != nil {
-					t.Fatalf("reading output metrics: %v", err)
-				}
-
-				for _, wanted := range wantedMetrics {
-					if !bytes.Contains(out, []byte(wanted+"{")) { // Add { to force an exact match.
-						t.Fatalf("Metric %q not found in output", wanted)
+				matchedMetrics := 0
+				for _, mf := range mfs {
+					if *mf.Name != tc.metricName {
+						// This is not the metric we are asserting on
+						continue
 					}
+
+				metric:
+					for _, m := range mf.Metric {
+						for _, labelPair := range m.Label {
+							// Check each label of this particular metric against the test case labels.
+							// If the metric has a label we're not matching for, that's okay, but it we are matching
+							// it then the value should match as well.
+							if actual, present := tc.metricLabels[*labelPair.Name]; present && actual != *labelPair.Value {
+								continue metric
+							}
+						}
+
+						matchedMetrics++
+						// Hack. Instead of check which type this metric has, and then use that one, rely on GetValue
+						// that does this check for us and return 0 if the type is not correct.
+						metricValue := m.Gauge.GetValue() + m.Counter.GetValue() + m.Untyped.GetValue()
+						if !tc.assertValue(metricValue) {
+							t.Fatalf("Metric value for %q got unexpected value %v (did not satisfy assert function)", *mf.Name, metricValue)
+						}
+					}
+				}
+
+				if matchedMetrics == 0 {
+					t.Fatalf("Test case for %q with specified labels matched no metric in extension output", tc.metricName)
 				}
 			})
 		}
 	})
+
+	t.Run("metrics have required prefix", func(t *testing.T) {
+		t.Parallel()
+
+		for _, m := range mfs {
+			if !strings.HasPrefix(*m.Name, "probe_") {
+				t.Fatalf("Metric %q not have the required prefix", *m.Name)
+			}
+		}
+	})
 }
 
-var wantedMetrics = []string{
-	"probe_data_received_bytes",
-	"probe_data_sent_bytes",
-	"probe_http_duration_seconds",
-	"probe_http_error_code",
-	"probe_http_got_expected_response",
-	"probe_http_info",
-	"probe_http_requests_failed_total",
-	"probe_http_requests_total",
-	"probe_http_ssl",
-	"probe_http_status_code",
-	"probe_http_total_duration_seconds",
-	"probe_http_version",
-	"probe_iteration_duration_seconds",
-	"probe_script_duration_seconds",
+func equals(expected float64) func(float64) bool {
+	return func(v float64) bool {
+		return v == expected
+	}
 }
 
-var scriptSingleRequest = []byte(`
-import http from 'k6/http';
+func nonZero(v float64) bool {
+	return v > 0
+}
 
-export const options = {
-  iterations: 1,
-};
-
-export default function () {
-  const response = http.get('https://test-api.k6.io/public/crocodiles/');
-}`)
+func any(float64) bool {
+	return true
+}
---
+++ integration/test-script.js
@@ -0,0 +1,45 @@
+import { check } from 'k6';
+import http from 'k6/http';
+import { Trend, Counter, Gauge } from 'k6/metrics';
+
+const testHost = __ENV.TEST_HOST ? __ENV.TEST_HOST : "test-api.k6.io";
+
+const myTrend = new Trend('waiting_time');
+const myCounter = new Counter('my_counter');
+const myGauge = new Gauge('my_gauge');
+
+export const options = {
+  iterations: 1,
+};
+
+export default function () {
+  myTrend.add(0.5);
+  myTrend.add(0.6);
+  myTrend.add(0.7);
+
+  myGauge.add(5);
+  myGauge.add(6); // Discards previous value.
+
+  myCounter.add(1);
+  myCounter.add(2);
+
+  check({}, {
+      'something': () => true,
+    }
+  );
+  check({}, {
+      'something': () => false,
+    }
+  );
+  check({}, {
+      'something': () => false,
+    }
+  );
+
+  http.get(`http://${testHost}`); // non-https.
+  http.get(`https://${testHost}/public/crocodiles/`);
+  http.get(`https://${testHost}/public/crocodiles2/`); // 404
+  http.get(`https://${testHost}/public/crocodiles3/`); // 404
+  http.get(`https://${testHost}/public/crocodiles4/`); // 404
+  http.get(`http://fail.internal/public/crocodiles4/`); // failed
+}
